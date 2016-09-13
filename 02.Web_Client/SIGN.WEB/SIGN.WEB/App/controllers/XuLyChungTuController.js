08c7905ea32a7f91594402fa4cfeacbd9202ef0d
huynm <huynm@beetsoft.com.vn>
2016-09-13 10:46:25 +0700

    update XuLyChungTu.cshtml

--- 02.Web_Client/SIGN.WEB/SIGN.WEB/App/controllers/XuLyChungTuController.js
+++ 02.Web_Client/SIGN.WEB/SIGN.WEB/App/controllers/XuLyChungTuController.js
@@ -1,9 +1,8 @@
 ﻿"use strict";
 
-app.controller('XuLyChungTuController', function ($scope, $rootScope, $sce, $windo0w, XuLyChungTuService, fileReader) {
+app.controller('XuLyChungTuController', function ($scope, $rootScope, $sce, $window, XuLyChungTuService, EdataService) {
     // Initial object
     $scope.doc = {};
-    $scope.newDoc = {};
     $scope.chungtuList = [];
 
     // Search documents
@@ -19,25 +18,4 @@ app.controller('XuLyChungTuController', function ($scope, $rootScope, $sce, $win
             });
     }
     $scope.searchDoc();
-
-    // Get file
-    $scope.file = null;
-    $scope.getFile = function (file, index) {
-        $scope.file = file;
-        fileReader.readAsDataUrl($scope.file, $scope)
-                      .then(function (result) {
-                          // Get url source
-                      });
-    };
-
-    // Create new doc
-    $scope.addNewDoc = function () {
-        var promisePost = EdataService.addNewDoc($scope.newDoc, $scope.file);
-        promisePost.then(
-            function (result) {
-
-            }, function (error) {
-                console.log(error);
-            });
-    }
 });
